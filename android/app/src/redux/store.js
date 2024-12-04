783aaafb6b1f2cc169658540b817c023f0e1761a
Muhammad Huzaifa <huzaifa.zenkoders@gmail.com>
2024-12-05 01:06:24 +0500

    persist

--- android/app/src/redux/store.js
+++ android/app/src/redux/store.js
@@ -1,10 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import counterSlice  from './features/counter/CounterSlice'
 import AuthSlice from './features/AuthSlice'
+import persistReducer from 'redux-persist/es/persistReducer'
+import persistStore from 'redux-persist/es/persistStore'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-export const store = configureStore({
-  reducer: {
-    counter: counterSlice,
+const reducers = combineReducers({
+  counter: counterSlice,
     auth: AuthSlice,
-  },
 })
+
+const persistConfig = {
+  key:'root',
+  storage: AsyncStorage,
+  whitelist : ['auth','counter']
+};
+const persistedReducer = persistReducer(persistConfig,reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware)=>
+    getDefaultMiddleware({serializableCheck: false}),
+});
+
+const persistor = persistStore(store);
+
+export {store,persistor};
--- index.js
+++ index.js
@@ -6,11 +6,14 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import { Provider } from 'react-redux';
-import { store } from './android/app/src/redux/store';
+import { persistor, store } from './android/app/src/redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const Root = () => (
     <Provider store={store}>
-        <App/>
+        <PersistGate loading = {null}  persistor={persistor}> 
+          <App/>
+        </PersistGate>
     </Provider>
 
 
